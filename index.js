d2067c04177515cb3d315b890c14c479af6f8248
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-10-23 17:03:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string) {
   return string.toUpperCase()
 }
 function logShout (string) {
-  console.log (string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 function whisper(string) {
   return string.toLowerCase()
