e64a1d8c9ceaf4e8dfabf3c6fa7a0325f15e119d
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-10-23 17:52:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
   var lowercase = "hello!"
   var mixedCase = "Hi there!"
-  if (string.toLowerCase() === lowercase) {
+  if (string == string.toLowerCase()) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === uppercase) {
     return "YES INDEED!"
